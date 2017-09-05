739d89fe73a319f0633b2171944b07f6e03c7400
NBAKH <hr.nbakh@gmail.com>
2017-09-05 12:00:40 +0200

    new file

--- openBrowser.py
+++ openBrowser.py
@@ -1,2 +1,3 @@
-import subprocess
-subprocess.call("epiphany-browser http://ddlab.au.dk", shell=True)
+#import subprocess
+#subprocess.call("epiphany-browser http://ddlab.au.dk", shell=True)
+-m webbrowser -t “http://ddlabau.github.io/openingHours”
